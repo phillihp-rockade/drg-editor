c4f6f5b73a52544629c4a8a7021168f6b877eee7
phillihp-rockade <zegeekof85@gmail.com>
2023-06-20 15:41:17 +0200

    Season 4

--- src/views/home/__tests__/homeView.tsx
+++ src/views/home/__tests__/homeView.tsx
@@ -4,7 +4,6 @@ import { HomeView } from '../homeView';
 describe('HomeView', () => {
   it('should render', () => {
     const tree = renderer.create(<HomeView />).toJSON();
-
     expect(tree).toMatchSnapshot();
   });
 });
